e9dcbc8ae63f956a962d2c5f0241164bc4cafe85
Eduardo Carrascosa <eduardoc.codes@gmail.com>
2026-01-26 18:22:38 -0300

    menu mobile

--- projeto-salao/js/script.js
+++ projeto-salao/js/script.js
@@ -12,3 +12,28 @@ window.addEventListener('scroll', () => {
 
   lastScroll = currentScroll;
 });
+
+// MENU CELULAR
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMenu = document.getElementById('close-menu');
+
+function openMenu() {
+  mobileMenu.classList.remove('-translate-y-full');
+  mobileMenu.classList.add('translate-y-0');
+  header.style.display = 'none';
+}
+
+function closeMobileMenu() {
+  mobileMenu.classList.add('-translate-y-full');
+  header.style.display = 'block';
+  mobileMenu.style.display = 'none';
+}
+
+menuBtn.addEventListener('click', openMenu);
+closeMenu.addEventListener('click', closeMobileMenu);
+
+mobileMenu.querySelectorAll('a').forEach(link => {
+  link.addEventListener('click', closeMobileMenu);
+});
+
